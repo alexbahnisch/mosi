e121b36cf6a6c1645cd0ce4b3fa10a835aeba118
Alex Bahnisch <alexbahnisch@gmail.com>
2017-08-17 21:03:41 +1000

    CD fixed, testing encryption again

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="mosi",
-    version="0.0.1.dev3",
+    version="0.0.1.dev4",
     description="Modelling & Optimization Solver Interface (MOSI).",
     url="https://github.com/alexbahnisch/mosi.py",
     author="Alex Bahnisch",
