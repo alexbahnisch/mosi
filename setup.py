a3435a388915eace9b7239bab5ac8d4791e9594d
Alex Bahnisch <alexbahnisch@gmail.com>
2017-09-03 05:19:52 +1000

    Corrected configuration

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ from setuptools import find_packages, setup
 
 setup(
     name="mosi",
-    version="0.0.2",
+    version="0.0.3",
     description="Modelling & Optimization Solver Interface (MOSI).",
     url="https://github.com/alexbahnisch/mosi.py",
     author="Alex Bahnisch",
     author_email="alexbahnisch@gmail.com",
     license="MIT",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
         "Natural Language :: English",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
@@ -29,12 +30,9 @@ setup(
     install_requires=[
         "future>=0.16.0"
     ],
-    setup_requires=[
-        "pytest-runner>=2.11.1",
-        "tox>=2.7.0"
-    ],
     tests_require=[
-        "pytest>=3.1.3"
+        "pytest>=3.1.3",
+        "pytest-runner>=2.11.1"
     ],
     test_suite="src.tests"
 )
